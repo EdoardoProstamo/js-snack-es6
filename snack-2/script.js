2ec8c934ec1b37942e96946d29995b255101684d
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-11 01:33:23 +0100

    feat: oggetti

--- snack-2/script.js
+++ snack-2/script.js
@@ -3,3 +3,36 @@
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squadra = [
+    {
+        nome: 'Inter',
+        pt: '93',
+        fouls: '114'
+    },
+    {
+        nome: 'Juventus',
+        pt: '76',
+        fouls: '92'
+    },
+    {
+        nome: 'Milan',
+        pt: '54',
+        fouls: '43'
+    },
+    {
+        nome: 'Roma',
+        pt: '69',
+        fouls: '74'
+    },
+    {
+        nome: 'Lazio',
+        pt: '91',
+        fouls: '111'
+    },
+    {
+        nome: 'Catania',
+        pt: '50',
+        fouls: '30'
+    }
+]
