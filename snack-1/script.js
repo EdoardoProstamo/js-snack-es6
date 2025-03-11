7152c99fe71649b70911bf6165472d80f1128622
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-11 01:03:13 +0100

    feat: oggetti creati

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,3 +1,31 @@
 // Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
+
+
+//PROCEDIMENTO
+// - creare gli oggetti nell'html
+//      - attribuire delle classi (o degli id) agli oggetti dell'html 
+// - creare un array in js
+//      -inserire gli oggetti
+//           - attribuire le proprietà ad ogni oggetto
+// - creare un if per stabilire quale oggetto ha il peso minore
+// - utilizzando il risultato dell'if, prendere l'oggetto con il peso minore
+// - stamparlo in console
+// - stamparlo sullo schermo tramite html
+
+
+const bicicletta = [
+    {
+        nome: 'bici 1',
+        peso: '21'
+    },
+    {
+        nome: 'bici 2',
+        peso: '44'
+    },
+    {
+        nome: 'bici 3',
+        peso: '11'
+    }
+];
